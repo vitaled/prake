d0d434e6b50cd4638bc2d0d6f88009e74813f18a
Daniele Vitale <dv@doodle.com>
2019-01-31 01:14:47 +0100

    fix: correct file indentation

--- prake/__init__.py
+++ prake/__init__.py
@@ -38,54 +38,54 @@ class Extractor:
 		last_candidate = []
 
 		text = text.strip().lower()
-    		text = re.sub(r"([\W])"," \g<1> ",text)
-    		text = re.sub(r"\s+"," ",text)
+		text = re.sub(r"([\W])"," \g<1> ",text)
+		text = re.sub(r"\s+"," ",text)
 
-    		tokens = re.split(r"[\s]",text)
+		tokens = re.split(r"[\s]",text)
 
-    		for token in tokens:
+		for token in tokens:
 
-        		if token not in self.stopwords and token not in self.phrase_delimiters and token !='':
+			if token not in self.stopwords and token not in self.phrase_delimiters and token !='':
 
-            			if token not in freq:
-                			freq[token]= 1
-            			else:
-                			freq[token]+=1
+				if token not in freq:
+					freq[token]= 1
+				else:
+					freq[token]+=1
 
-            			last_candidate.append(token)
+				last_candidate.append(token)
 
-		        elif len(last_candidate)>0:
-            			candidate_keywords.append(last_candidate)
+			elif len(last_candidate)>0:
+				candidate_keywords.append(last_candidate)
 				# compute degree
 				for keyword in last_candidate:
-                			if keyword not in deg:
-                    				deg[keyword] = len(last_candidate)-1
-                			else:
-                    				deg[keyword]+= len(last_candidate)-1
+							if keyword not in deg:
+									deg[keyword] = len(last_candidate)-1
+							else:
+									deg[keyword]+= len(last_candidate)-1
 				last_candidate = []
 
 		if(len(last_candidate) > 0):
-    				candidate_keywords.append(last_candidate)
-				for keyword in last_candidate:
-	               			if keyword not in deg:
-                    				deg[keyword] = len(last_candidate)-1
-                			else:
-                    				deg[keyword]+= len(last_candidate)-1
+			candidate_keywords.append(last_candidate)
+			for keyword in last_candidate:
+				if keyword not in deg:
+					deg[keyword] = len(last_candidate)-1
+				else:
+					deg[keyword]+= len(last_candidate)-1
 
 
 		scores = {}
 		for ck in candidate_keywords:
-    			score = 0.0
-	    		for token in ck:
-        			score += float(deg[token]+freq[token])/float(freq[token])
-    				scores[" ".join(ck)] = score
-
-    		return sorted(scores.items(),key=operator.itemgetter(1),reverse=True)[0:int(len(scores)*self.threshold)]
+			score = 0.0
+			for token in ck:
+				score += float(deg[token]+freq[token])/float(freq[token])
+				scores[" ".join(ck)] = score
+		
+		return sorted(scores.items(),key=operator.itemgetter(1),reverse=True)[0:int(len(scores)*self.threshold)]
 
 	def load_words(self,path):
 		f = codecs.open(path,"r","utf-8")
-    		words = set()
-    		for line in f:
-        		words.add(line.strip())
-    		f.close()
-    		return words
+		words = set()
+		for line in f:
+			words.add(line.strip())
+		f.close()
+		return words
